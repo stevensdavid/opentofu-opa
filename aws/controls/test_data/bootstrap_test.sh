bc842c815d8ff5ca5d705ec7687d20e336b48b43
David Stevens <gpg@davidstevens.se>
2025-05-30 14:50:27 +0200

    Expand bootstrap to set up rego files

--- aws/controls/test_data/bootstrap_test.sh
+++ aws/controls/test_data/bootstrap_test.sh
@@ -28,6 +28,66 @@ control_id="aws.controls.$service.$control_number"
 
 function_name="evaluate_${service}_${control_number}"
 
+mkdir -p "../${service}"
+# Create missing files
+if [ ! -f "../${service}/main.rego" ]; then
+    cat <<EOF >>"../${service}/main.rego"
+package aws.controls.$service
+
+import rego.v1
+
+evaluate(plan) := union({})
+EOF
+fi
+if [ ! -f "../${service}/main_test.rego" ]; then
+    cat <<EOF >>"../${service}/main_test.rego"
+package aws.controls.${service}_test
+
+import rego.v1
+
+import data.aws.controls
+import data.aws.controls.${service}
+
+test_evaluate_includes_all_rules if {
+	every rule_id in object.keys(controls.mocks.${service}) {
+		opa_rule_id := sprintf("aws.controls.${service}.%s", [rule_id])
+		denies := ${service}.evaluate(controls.mocks.${service}[rule_id].fail)
+		some deny in denies
+		deny.id.opa == opa_rule_id
+
+		permits := ${service}.evaluate(controls.mocks.${service}[rule_id].pass)
+		every unrelated_deny in permits {
+			unrelated_deny.id.opa != opa_rule_id
+		}
+	}
+}
+EOF
+fi
+
+if [ ! -f "../${service}/policies.rego" ]; then
+    echo "Writing to ../${service}/policies.rego"
+    cat <<EOF >>"../${service}/policies.rego"
+package aws.controls.${service}
+
+import data.aws.utils as aws_utils
+import data.utils
+
+import rego.v1
+EOF
+fi
+
+if [ ! -f "../${service}/policies_test.rego" ]; then
+    echo "Writing to ../${service}/policies_test.rego"
+    cat <<EOF >>"../${service}/policies_test.rego"
+package aws.controls.${service}_test
+
+import rego.v1
+
+import data.aws.controls
+import data.aws.controls.${service}
+EOF
+fi
+
 cat <<EOF >>"../${service}/policies_test.rego"
 
 test_${function_name}_valid_input if count(${service}.$function_name(controls.mocks.${service}["$control_number"].pass)) == 0
